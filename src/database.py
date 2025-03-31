65ff7337ae03ef668000b8794b2f2c067194bc41
Alexey Stolbov <alstoff@live.ru>
2025-03-31 13:52:22 +0300

    исправлен модуль работы с БД

--- src/database.py
+++ src/database.py
@@ -2,36 +2,31 @@ import psycopg2
 from src.config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST
 
 def create_database():
-    conn = psycopg2.connect(dbname="postgres", user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
     conn.autocommit = True
     cur = conn.cursor()
-    cur.execute(f"DROP DATABASE IF EXISTS {DB_NAME}")
-    cur.execute(f"CREATE DATABASE {DB_NAME}")
-    cur.close()
-    conn.close()
 
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
-    cur = conn.cursor()
     cur.execute("""
-        CREATE TABLE employers (
+        CREATE TABLE IF NOT EXISTS employers (
             id SERIAL PRIMARY KEY,
             hh_id INT UNIQUE NOT NULL,
-            name VARCHAR(255) NOT NULL
-        )
+            name TEXT NOT NULL
+        );
     """)
+
     cur.execute("""
-        CREATE TABLE vacancies (
+        CREATE TABLE IF NOT EXISTS vacancies (
             id SERIAL PRIMARY KEY,
-            employer_id INT REFERENCES employers(id),
-            title VARCHAR(255) NOT NULL,
-            salary INT,
+            employer_id INTEGER REFERENCES employers(id),
+            title TEXT NOT NULL,
+            salary INTEGER,
             url TEXT
-        )
+        );
     """)
+
     conn.commit()
     cur.close()
     conn.close()
-
 
 def reset_tables():
     conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
@@ -41,4 +36,3 @@ def reset_tables():
     conn.commit()
     cur.close()
     conn.close()
-
